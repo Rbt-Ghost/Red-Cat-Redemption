494c2610a3b7debd42f67d4febf7ee60092f16d7
Robert <robertnostor@gmail.com>
2025-11-18 11:46:01 +0200

    Add FadeRenderer for dynamic sprite fading

---
+++ Assets/Scripts/StaticObjects/FadeRenderer.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class FadeRenderer : MonoBehaviour
+{
+    const float fadeInAlpha = 1f;
+    const float fadeOutAlpha = 0.2f;
+
+    private bool isFading = false;
+    private SpriteRenderer[] spriteRenderers; // Array to hold all SpriteRenderers in children
+
+    void Start()
+    {
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (isFading)
+        {
+            FadeOut();
+        }
+        else
+        {
+            FadeIn();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isFading) // to prevent multiple triggers
+            return;
+        if (collision.TryGetComponent<PlayerStats>(out var playerStats)) // calling the PlayerStats script from the player object
+        {
+            //Debug.Log("OnTriggerEnter2D ");
+            isFading = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!isFading) // to prevent multiple triggers
+                return; 
+        if (collision.TryGetComponent<PlayerStats>(out var playerStats)) // calling the PlayerStats script from the player object
+        {
+            //Debug.Log("OnTriggerExit2D ");
+            isFading = false;
+        }
+    }
+
+    private void FadeOut()
+    {
+        foreach( var rederer in spriteRenderers )
+        {
+            changeOpacity(rederer, fadeOutAlpha);
+        }
+    }
+
+    private void FadeIn()
+    {
+        foreach (var rederer in spriteRenderers)
+        {
+            changeOpacity(rederer, fadeInAlpha);
+        }
+    }
+
+    private void changeOpacity(SpriteRenderer obj, float targetAlpha)
+    {
+        Color color = obj.color;
+        Color smoothColor = new Color(color.r, color.g, color.b, Mathf.Lerp(color.a, targetAlpha, Time.deltaTime * 5f)); // Smooth transition
+        obj.color = smoothColor;
+    }
+}
