05e7fe2c9c1d093cd0511fd4b670a25b8c8ecc62
Ilisoi Fineas <fineasilisoi1@gmail.com>
2025-11-20 21:39:17 +0200

    meniu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(string sceneName)
+
+public void PlayGame(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName);
     }
@@ -14,4 +16,37 @@ public class MainMenu : MonoBehaviour
         Debug.Log("game quit");
     }
 
+
+    public GameObject settingPanelShadow; 
+    public GameObject settingPanel;      
+
+    
+    public void SetPanelsTransparency()
+    {
+        
+        if (settingPanelShadow != null)
+        {
+            Image shadowImg = settingPanelShadow.GetComponent<Image>();
+            if (shadowImg != null)
+            {
+                Color c = shadowImg.color;
+                c.a = 0.5f; 
+                shadowImg.color = c;
+            }
+        }
+
+   
+        if (settingPanel != null)
+        {
+            Image panelImg = settingPanel.GetComponent<Image>();
+            if (panelImg != null)
+            {
+                Color c = panelImg.color;
+                c.a = 1f; 
+                panelImg.color = c;
+            }
+        }
+    }
+
 }
+
