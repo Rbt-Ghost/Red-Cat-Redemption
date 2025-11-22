00b509f58e3abed09972d7cdd7ad50935a15b6d5
Ilisoi Fineas <fineasilisoi1@gmail.com>
2025-11-22 13:16:55 +0200

    menu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -3,15 +3,27 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
-   
-public void PlayGame(string sceneName)
+    public Animator transition;
+    public float transitionTime = 1f;
+    public void LoadNextLevel()
     {
-      
-        SceneManager.LoadSceneAsync(sceneName);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+    
 
     public void QuitGame()
     {
